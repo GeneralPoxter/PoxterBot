3a1a1a3ee9c62baf628b2242b71489c09620585f
GeneralPoxter <generalpoxter@gmail.com>
2021-06-22 14:49:17 -0400

    update script

--- rebuild-poxter.sh
+++ rebuild-poxter.sh
@@ -1,5 +1,7 @@
-docker stop poxterbot
-docker rm poxterbot
+if [ "$(docker ps -a | grep poxterbot)" ]; then
+    docker stop poxterbot
+    docker rm poxterbot
+fi
 # cd [PoxterBot directory]
 docker-compose build
 docker run --name poxterbot --restart always poxterbot
